3a4c7cd4d9a83763a757ec99df5d24d1b19f1681
hewei <jashewe@ixonos.com>
2011-03-10 10:43:43 +0800

    add a new thread to control the auto pause function

---
+++ EnglishCentral/src/com/ixonos/MediaControlThread.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package com.ixonos;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+/**
+ * @author jashewe
+ * 
+ */
+public class MediaControlThread extends Thread {
+
+	private CaptionsUtil mUtil;
+	private int mIndex;
+	private Handler mHandler;
+	private long mCurrentMillSec;
+
+	public MediaControlThread(CaptionsUtil util, int index, Handler handler,
+			long currentMillSec) {
+
+		this.mUtil = util;
+		this.mIndex = index;
+		this.mHandler = handler;
+		this.mCurrentMillSec = currentMillSec;
+	}
+
+	@Override
+	public void run() {
+
+		Log.v("MediaControlThread", ""+mUtil.getSentences().get(mIndex).getToTime()+" "+mCurrentMillSec+" "+mIndex);
+		long sleepMilSec = mUtil.getSentences().get(mIndex+1).getFromTime()
+				- mCurrentMillSec;
+		try {
+
+			if(sleepMilSec>0){
+
+				Thread.sleep(sleepMilSec);
+				Message message = new Message();
+				message.what = MianActivity.PAUSE_MSG;
+				Bundle data = new Bundle();
+
+				// set the current index into data:
+				data.putInt(MianActivity.CAPTION_MSG, mIndex);
+				message.setData(data);
+				mHandler.sendMessage(message);
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private int getNowSentenceIndex(long t) {
+
+		for (int i = 0; i < mUtil.getSentences().size(); i++) {
+
+			if (mUtil.getSentences().get(i).isInTime(t)) {
+
+				return i;
+			}
+		}
+		return -1;
+	}
+}
